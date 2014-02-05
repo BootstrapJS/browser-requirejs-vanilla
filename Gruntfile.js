4794d30f50546d9600014e2717bcceca5895cf2f
Jakob Westhoff <jakob@westhoffswelt.de>
2014-02-05 21:20:03 +0100

    Added basic symlink and test page setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,25 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     /**
+     * Basic configuration for all watch tasks
+     */
+    grunt.config("watch", {
+        options: {
+            atBegin: true
+        }
+    });
+
+    /**
+     * Basic configuration for all symlink tasks
+     */
+    grunt.config("symlink", {
+        options: {
+            overwrite: true
+        }
+    });
+
+
+    /**
      * Jshint configuration for linting the project files
      */
     grunt.config("jshint", {
@@ -18,6 +37,12 @@ module.exports = function (grunt) {
             "src/**/*.js"
         ]
     });
+
+    grunt.config("watch.lint", {
+        files: grunt.config.get("jshint.all"),
+        tasks: ["lint"]
+    });
+
 
     /**
      * Statically build all dependencies into one file for production
@@ -52,24 +77,34 @@ module.exports = function (grunt) {
         }
     });
 
-    /**
-     * Allow to watch and re-execute tasks once files change automatically
-     */
-    grunt.config("watch", {
-        options: {
-            atBegin: true
-        },
-        lint: {
-            files: grunt.config.get("jshint.all"),
-            tasks: ["lint"]
-        },
-        build: {
-            files: [
-                "src/**/*.js"
-            ]
-        }
+    grunt.config("watch.build", {
+        files: [
+            "src/**/*.js"
+        ]
     });
 
+
+    /**
+     * Symlink files to www directory
+     */
+    grunt.config("symlink.www", {
+        files: [
+            {
+                src: "node_modules/requirejs/require.js",
+                dest: "www/vendor/requirejs/require.js"
+            },
+            {
+                src: "src",
+                dest: "www/js"
+            },
+            {
+                expand: true,
+                cwd: "assets",
+                src: ["*"],
+                dest: "www/"
+            }
+        ]
+    });
 
     /**
      * Available Grunt-Tasks to the outside world
---
+++ src/main.js
@@ -0,0 +1,3 @@
+define(function() {
+    alert("I am ready and loaded!");
+});
